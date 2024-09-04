ad71d7150135d77ef7f1ec8d28b7e62694f2f2cb
rizwan <therizwanulhaq@gmail.com>
2024-09-04 23:02:35 +0530

    dummy data

---
+++ lib/data/posts.dart
@@ -0,0 +1,170 @@
+import 'package:mindlink/models/post.dart';
+
+final List<Post> posts = [
+  Post(
+    id: "1",
+    username: 'user1',
+    avatar: 'https://robohash.org/cumqueidet.png?size=50x50&set=set1',
+    content: 'This is the content of the first post.',
+    type: 'text',
+  ),
+  Post(
+    id: "2",
+    username: 'user2',
+    avatar: 'https://robohash.org/illodoloresdolorem.png?size=50x50&set=set1',
+    content: 'Check out this amazing sunset I captured.',
+    type: 'image',
+    mediaUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Anatomy_of_a_Sunset-2.jpg/300px-Anatomy_of_a_Sunset-2.jpg',
+  ),
+  Post(
+    id: "3",
+    username: 'user3',
+    avatar: 'https://robohash.org/cupiditateenimomnis.png?size=50x50&set=set1',
+    content: 'I just got a new job!',
+    type: 'text',
+  ),
+  Post(
+    id: "4",
+    username: 'user4',
+    avatar:
+        'https://robohash.org/velvoluptatemsimilique.png?size=50x50&set=set1',
+    content: 'This is interesting.',
+    type: 'video',
+    mediaUrl:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
+  ),
+  Post(
+    id: "5",
+    username: 'user5',
+    avatar: 'https://robohash.org/aliasquiiusto.png?size=50x50&set=set1',
+    content: 'Meet my new puppy, Bella!',
+    type: 'image',
+    mediaUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Golde33443.jpg/220px-Golde33443.jpg',
+  ),
+  Post(
+    id: "6",
+    username: 'user6',
+    avatar: 'https://robohash.org/etquisneque.png?size=50x50&set=set1',
+    content: 'Happiness is a journey, not a destination.',
+    type: 'text',
+  ),
+  Post(
+    id: "7",
+    username: 'user7',
+    avatar:
+        'https://robohash.org/perspiciatisveritatisut.png?size=50x50&set=set1',
+    content: 'Loving my time at the beach!',
+    type: 'image',
+    mediaUrl:
+        'https://www.101holidays.co.uk/wp-content/uploads/2008/11/BeachHouse-Maldives-238x238.jpg',
+  ),
+  Post(
+    id: "8",
+    username: 'user8',
+    avatar: 'https://robohash.org/cumestquia.png?size=50x50&set=set1',
+    content: 'Let’s discuss the latest in AI technology.',
+    type: 'text',
+  ),
+  Post(
+    id: "9",
+    username: 'user9',
+    avatar: 'https://robohash.org/liberodoloremid.png?size=50x50&set=set1',
+    content: 'Subaru Outback On Street And Dirt',
+    type: 'video',
+    mediaUrl:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4',
+  ),
+  Post(
+    id: "10",
+    username: 'user10',
+    avatar: 'https://robohash.org/cumestquia.png?size=50x50&set=set1',
+    content: 'Starting the day with a cup of coffee.',
+    type: 'image',
+    mediaUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/800px-A_small_cup_of_coffee.JPG',
+  ),
+  Post(
+    id: "11",
+    username: 'user11',
+    avatar: 'https://robohash.org/illodoloresdolorem.png?size=50x50&set=set1',
+    content: 'Starting my fitness progress.',
+    type: 'text',
+  ),
+  Post(
+    id: "12",
+    username: 'user12',
+    avatar: 'https://robohash.org/etquisneque.png?size=50x50&set=set1',
+    content: 'Enjoying a peaceful walk in the park.',
+    type: 'image',
+    mediaUrl:
+        'https://static01.nyt.com/images/2013/03/26/health/26well_physed/26well_physed-superJumbo.jpg',
+  ),
+  Post(
+    id: "13",
+    username: 'user13',
+    avatar: 'https://robohash.org/cumestquia.png?size=50x50&set=set1',
+    content: 'Working on a new project in Flutter!',
+    type: 'text',
+  ),
+  Post(
+    id: "14",
+    username: 'user14',
+    avatar: 'https://robohash.org/etquisneque.png?size=50x50&set=set1',
+    content: 'Captured this moment in the city.',
+    type: 'image',
+    mediaUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/July_night_sky_%2835972569256%29.jpg/280px-July_night_sky_%2835972569256%29.jpg',
+  ),
+  Post(
+    id: "15",
+    username: 'user15',
+    avatar: 'https://robohash.org/cupiditateenimomnis.png?size=50x50&set=set1',
+    content: 'We Are Going On Bull run',
+    type: 'video',
+    mediaUrl:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4',
+  ),
+  Post(
+    id: "16",
+    username: 'user16',
+    avatar: 'https://robohash.org/illodoloresdolorem.png?size=50x50&set=set1',
+    content: 'Just finished a challenging trek!',
+    type: 'image',
+    mediaUrl:
+        'https://harstuff-travel.org/wp-content/uploads/2019/03/LRM_EXPORT_929102018503574_20190214_142502283.jpeg',
+  ),
+  Post(
+    id: "17",
+    username: 'user17',
+    avatar: 'https://robohash.org/cumestquia.png?size=50x50&set=set1',
+    content: 'You should read this amazing book!',
+    type: 'text',
+  ),
+  Post(
+    id: "18",
+    username: 'user18',
+    avatar: 'https://robohash.org/illodoloresdolorem.png?size=50x50&set=set1',
+    content: 'Big buck bunny',
+    type: 'video',
+    mediaUrl:
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+  ),
+  Post(
+    id: "19",
+    username: 'user19',
+    avatar: 'https://robohash.org/cupiditateenimomnis.png?size=50x50&set=set1',
+    content: 'Check out my latest artwork!',
+    type: 'image',
+    mediaUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Great_Wave_off_Kanagawa2.jpg/220px-Great_Wave_off_Kanagawa2.jpg',
+  ),
+  Post(
+    id: "20",
+    username: 'user20',
+    avatar: 'https://robohash.org/cumestquia.png?size=50x50&set=set1',
+    content: 'Relaxing and unwinding this weekend.',
+    type: 'text',
+  ),
+];
