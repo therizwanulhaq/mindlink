009dc8a9ed76c22874956a408666a72c1c3867ad
rizwan <therizwanulhaq@gmail.com>
2024-09-04 12:46:19 +0530

    dynamically switching screens using index

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:mindlink/screens/image_post_screen.dart';
+import 'package:mindlink/screens/text_post_screen.dart';
+import 'package:mindlink/screens/video_post_screen.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedPageIndex = 0;
+
+  final List<Widget> _pages = [
+    const TextPostScreen(),
+    const ImagePostScreen(),
+    const VideoPostScreen(),
+  ];
+
+  final List<String> _pageTitles = [
+    "Text Post",
+    "Image Post",
+    "Video Post",
+  ];
+
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text("yahoo"),
+      appBar: AppBar(
+        title: Text(_pageTitles[_selectedPageIndex]),
       ),
+      body: _pages[_selectedPageIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedPageIndex,
+        onTap: _selectedPage,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.text_snippet),
---
+++ lib/screens/image_post_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ImagePostScreen extends StatelessWidget {
+  const ImagePostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Images"),
+    );
+  }
+}
