f9f59e2068ef37471d0c95945348be4a3fb08c3a
rizwan <therizwanulhaq@gmail.com>
2024-09-05 21:54:22 +0530

    added new fields to post model and redesigned text posts

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Post {
   final String id;
+  final String name;
   final String username;
   final String avatar;
   final String content;
@@ -9,6 +10,7 @@ class Post {
 
   Post({
     required this.id,
+    required this.name,
     required this.username,
     required this.avatar,
     required this.content,
--- lib/screens/text_post_screen.dart
+++ lib/screens/text_post_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mindlink/data/posts.dart';
 import 'package:mindlink/models/post.dart';
 
@@ -13,8 +14,69 @@ class TextPostScreen extends StatelessWidget {
     return ListView.builder(
       itemCount: textPosts.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(textPosts[index].content),
+        return Container(
+          decoration: const BoxDecoration(
+              border: Border(
+                  top: BorderSide(
+                      width: 1, color: Color.fromARGB(129, 196, 196, 196)))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+                leading: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: NetworkImage(textPosts[index].avatar),
+                  backgroundColor: Colors.grey[200],
+                ),
+                title: Text(
+                  textPosts[index].name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(
+                  "@${textPosts[index].username}",
+                  style: const TextStyle(color: Colors.grey),
+                ),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.more_vert,
+                      size: 20,
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 62),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      textPosts[index].content,
+                    ),
+                    Row(
+                      children: [
+                        const Icon(FontAwesomeIcons.heart, size: 15),
+                        const SizedBox(width: 16),
+                        const Icon(FontAwesomeIcons.comment, size: 15),
+                        const SizedBox(width: 16),
+                        const Icon(FontAwesomeIcons.retweet, size: 15),
+                        const Spacer(),
+                        const Icon(FontAwesomeIcons.bookmark, size: 15),
+                        const SizedBox(width: 16),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.share,
+                              size: 15,
+                            )),
+                        const SizedBox(width: 10),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
